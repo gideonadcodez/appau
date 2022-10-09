42bc5b3dbb3f97576e7de09539057216f11ed181
Gideon Appau <gideonad.codez@gmail.com>
2022-10-09 17:03:28 +0000

    Major Tweaks

--- main.js
+++ main.js
@@ -1,35 +1,26 @@
-
 (function () {
-  let blogBtn = document.getElementById("blog");
-  blogBtn.addEventListener("click", () => {
-    let blogToast = document.querySelector(".blog-toast");
-    blogToast.classList.toggle("appear");
-  });
-})();
-
-(function () {
-  let theme = localStorage.getItem("data-theme");
-  const toggle = document.getElementById("toggle");
-  const changeThemeToDark = () => {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("data-theme", "dark");
-  };
-
-  const changeThemeToLight = () => {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("data-theme", "light");
-  };
-
-  if (theme === "dark") {
-    changeThemeToDark();
-  }
-
-  toggle.addEventListener("click", () => {
     let theme = localStorage.getItem("data-theme");
+    const toggle = document.getElementById("toggle");
+    const changeThemeToDark = () => {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("data-theme", "dark");
+    };
+  
+    const changeThemeToLight = () => {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("data-theme", "light");
+    };
+  
     if (theme === "dark") {
-      changeThemeToLight();
-    } else {
       changeThemeToDark();
     }
-  });
-})();
+  
+    toggle.addEventListener("click", () => {
+      let theme = localStorage.getItem("data-theme");
+      if (theme === "dark") {
+        changeThemeToLight();
+      } else {
+        changeThemeToDark();
+      }
+    });
+  })();
