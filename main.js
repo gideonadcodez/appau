2b1f39f1e75ab573b619e484978871a556591a0b
Gideon Appau <gideonad.codez@gmail.com>
2023-02-09 23:47:24 -0800

    Minor Tweaks

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 (function () {
+  let theme = localStorage.getItem("data-theme");
+  const toggle = document.getElementById("toggle");
+  const changeThemeToDark = () => {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+  };
+
+  const changeThemeToLight = () => {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("data-theme", "light");
+  };
+
+  if (theme === "dark") {
+    changeThemeToDark();
+  }
+
+  toggle.addEventListener("click", () => {
     let theme = localStorage.getItem("data-theme");
-    const toggle = document.getElementById("toggle");
-    const changeThemeToDark = () => {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("data-theme", "dark");
-    };
-  
-    const changeThemeToLight = () => {
-      document.documentElement.setAttribute("data-theme", "light");
-      localStorage.setItem("data-theme", "light");
-    };
-  
     if (theme === "dark") {
+      changeThemeToLight();
+    } else {
       changeThemeToDark();
     }
-  
-    toggle.addEventListener("click", () => {
-      let theme = localStorage.getItem("data-theme");
-      if (theme === "dark") {
-        changeThemeToLight();
-      } else {
-        changeThemeToDark();
-      }
-    });
-  })();
+  });
+})();
+// When the user scrolls the page, execute myFunction
+window.onscroll = myFunction;
+// Get the navbar
+var navbar = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
