adfb8f1b80984133c2f4a5c6a3ada0e090d94e1f
Gideon Appau <gideonad.codez@gmail.com>
2023-02-14 06:41:45 -0800

    initial commit

--- main.js
+++ main.js
@@ -24,23 +24,3 @@
     }
   });
 })();
-// When the user scrolls the page, execute myFunction
-window.onscroll = myFunction;
-// Get the navbar
-var navbar = document.getElementById("topnav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  const toggle = document.getElementById("toggle");
-
-  if (window.pageYOffset >= sticky) {
-    toggle.style.marginRight = "-2000px";
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-    toggle.style.marginRight = "10px";
-  }
-}
